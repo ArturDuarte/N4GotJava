e2675a74113d51d55a168cdd74c8e6c6cdfd40f0
artco <artco@Artbook>
2023-08-31 09:12:10 -0400

    Inicio preparação txt

---
+++ N4Got/src/br/com/artcomp/controller/ManipulaTXT.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.artcomp.controller;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author artco
+ */
+public class ManipulaTXT {
+    
+    
+    public static void leitor(String path) throws IOException {
+		BufferedReader buffRead = new BufferedReader(new FileReader(path));
+		String linha = "";
+		while (true) {
+			if (linha != null) {
+				System.out.println(linha);
+
+			} else
+				break;
+			linha = buffRead.readLine();
+		}
+		buffRead.close();
+	}
+
+	public static void escritor(String path) throws IOException {
+		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
+		String linha = "";
+		Scanner in = new Scanner(System.in);
+		System.out.println("Escreva algo: ");
+		linha = in.nextLine();
+		buffWrite.append(linha + "\n");
+		buffWrite.close();
+	}
+}
